6a216a460959e8fefda917471f8ef154a16b071e
jainshubh9301 <jainshubh1239510@gmail.com>
2024-10-01 04:38:35 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+let face = prompt("your name !");
+let umra = prompt("your age");
+let jagah = prompt("your city");
+
+document.getElementById("N").innerHTML= "Name =" + face ;
+document.getElementById("A").innerHTML = "Age =" + umra ;
+document.getElementById("C").innerHTML = "City =" + jagah ;
+
+
+document.getElementById("name").innerHTML = "Name =" + face ;
+document.getElementById("age").innerHTML = "age =" + umra ;
+document.getElementById("city").innerHTML = "city =" + jagah ;
+
+
+
+
+
+
+
